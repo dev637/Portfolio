98431e68bdd1833f7a6dfaec22ea87ede3f061c1
Dev <devang713@gmail.com>
2018-04-22 10:18:39 -0400

    Adjust offset of smooth scroll links

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ var $root = $('html, body');
 
 $('a[href^="#"]').click(function () {
     $root.animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top - nav.offsetHeight + 1
+        scrollTop: $( $.attr(this, 'href') ).offset().top
     }, 800, 'swing');
     return false;
 });
